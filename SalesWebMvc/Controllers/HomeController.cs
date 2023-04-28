4897a0f569479aa014f87708441721be0e67561d
Allan Almeida <allansoares.almeida@gmail.com>
2023-04-27 22:44:14 -0300

    Primeiro controller e paginas razor

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC app from c# course";
+            ViewData["Professor"] = "Nelio Alves";
 
             return View();
         }
